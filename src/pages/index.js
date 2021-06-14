9bd4ed86b245e04227a114a65185a38387f71807
Junaid-Rasheed <junaidrasheed300@gmail.com>
2021-06-14 23:12:25 +0500

    netlify-server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!Junaid</div>
+  const [data, setdata] = useState("")
+
+  useEffect(async () => {
+    const responce = await fetch(".netlify/functions/hello")
+    const output = await responce.json()
+    setdata(output)
+  }, [])
+
+  return (
+    <div>
+      <div>
+        <h2>data is {data.message}</h2>
+      </div>
+      Hello world!Junaid
+    </div>
+  )
 }
